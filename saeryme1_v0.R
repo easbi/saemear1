64bbce500a29475233a3235884c1f7f35486bbfd
easbi <easbi@bps.go.id>
2021-04-19 10:22:03 +0700

    delete the application script and move to testfungsi.R

--- saeryme1_v0.R
+++ saeryme1_v0.R
@@ -196,38 +196,5 @@ saeryme1 = function(formula, t, area, w, data, ME=TRUE){
   return(res)
 }
 
-dt_test=import("Data_2 - test.xlsx") %>% mutate(your_x2=scale(your_x2))
-colnames(dt_test)
-
-# Application without Measurement Error
-out=saeryme1(
-  formula = your_y_star~your_x1+your_x2,
-  t = ~your_t,
-  area = ~your_Area,
-  w = ~your_w,
-  data = dt_test, 
-  ME = F
-)
-
-out$est
-out$REML
-out$beta
-
-# Application with Measurement Error
-out_ME=saeryme1(
-  formula = your_y_star~your_x1+your_x2,
-  t = ~your_t,
-  area = ~your_Area,
-  w = ~your_w,
-  data = dt_test, 
-  ME = T
-)
-
-out_ME$est
-out_ME$REML
-out_ME$beta
-
-plot(out$est$est, type="l")
-lines(out_ME$est$est, col="red")
 
 
